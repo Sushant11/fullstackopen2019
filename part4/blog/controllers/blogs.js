a2f300f8e91314b4e4c3fb62853d3ff905a1cea2
Sushant11 <sushant.chitrakar@citytech.global>
2020-01-08 17:45:00 +0545

    4b

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -12,9 +12,28 @@ blogsRouter.get("/",async (request, response) => {
 blogsRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
 
-  blog.save().then(result => {
-    response.status(201).json(result);
-  });
+  const post = await blog.save()
+  response.status(201).json(post)
+  // blog.save().then(result => {
+  //   response.status(201).json(result);
+  // });
 });
+
+blogsRouter.delete("/:id", async (request,response) => {
+  const id = request.params.id
+  const blogs = await Blog.find({})
+  blogs.find(blog => blog.id === id).remove()
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const update = {
+    likes: body.likes
+  }
+  const updated = await Blog.findByIdAndUpdate(request.params.id, update, {new: true})
+  console.log('updated :', updated);
+  response.status(201).json(updated);
+})
 
 module.exports = blogsRouter
