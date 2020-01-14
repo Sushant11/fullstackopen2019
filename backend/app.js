a891f859588ea158f2f73195efa21ef1e3df7a38
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-01-14 22:26:27 +0545

    fix

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,10 @@ const usersRouter = require("./controllers/users");
 const loginRouter = require('./controllers/login');
 const mongoose = require("mongoose");
 
-console.log("connecting to", config.MONGODB_URI);
+console.log("connecting to", MONGODB_URI);
 
 mongoose
-  .connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
     console.log("connected to MongoDB");
   })
